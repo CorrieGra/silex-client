fc2c37031dcb73faff6f17c8517fe5b2fea41b69
Corrie Graham <corriegraham@Corries-MacBook-Air.local>
2021-10-20 18:38:50 +0200

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { StoreProvider } from 'easy-peasy;'
+import App from './components/App/App';
+import { store } from './store/index';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <StoreProvider store={ store }>
+      <App />
+    </StoreProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
