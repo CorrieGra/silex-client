e743b3c13bef6fcfbf0687860af80f9598f71a63
Corrie Graham <corriegraham@Corries-MacBook-Air.local>
2021-10-21 08:50:52 +0200

    chore: added thunk for api call to load data

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,22 @@
-import clues from '../clues.json';
-import { action, createStore, persist } from 'easy-peasy';
+import axios from 'axios';
+import { action, createStore, persist, thunk } from 'easy-peasy';
 
 export const store = createStore(
     persist({
-        clues,
+        clues: [],
         currentClue: 0,
         score: 0,
+        updateClues: action((state, clues) => {
+            state.clues.push(...clues);
+        }),
+        getClues: thunk(async (actions) => {
+            const { data } = await axios({
+                url: `https://jservice.io/api/clues`,
+                method: 'GET'
+            });
+
+            actions.updateClues(data);
+        }),
         updateScore: action((state, score) => {
             state.score += score;
         }),
